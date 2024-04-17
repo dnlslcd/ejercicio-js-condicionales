83b211eff5aa8e8009842ff87a9750e66538b0d4
dnlslcd <danielsalcedoq@gmail.com>
2024-04-17 21:34:38 +0200

    ex3 acabado

--- ex3.js
+++ ex3.js
@@ -1,10 +1,11 @@
 /*
 1. Comprueba que no se muestra nada por el terminal con "node ex3.js"
 2. Asigna un valor a la variable "numero" para que se muestren por el terminal TODOS los console.log del código. Actualmente, estamos asignando un 0.
-3. BONUS: ¿Cual es el valón mínimo entero que resuelve este ejercicio?
-*/
+3. BONUS: ¿Cual es el valor mínimo entero que resuelve este ejercicio?
+* 501
+*/ 
 
-let numero = 0;
+let numero = 501;
 
 if (numero > 0) {
   console.log("El número es mayor de 0.");
